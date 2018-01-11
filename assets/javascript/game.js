4feaad2d1336eed69d86581926d5d8f17ba466ab
Joy Curth <joy.curth@comcast.net>
2018-01-10 22:14:52 -0500

    removed break in html and center align in css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -73,10 +73,11 @@
 						if (totalUserNumber > computerRandomNumber){
 
 								losses ++;
-								 // $(".Losses .value").html(losses);
 								var lossestext = document.getElementById("losses");
 								document.getElementById("Losses").innerHTML = 
 								"Losses: " + losses;
+
+								// $("#losses").html("Losses: " + losses);
 
 
 								newgame();
