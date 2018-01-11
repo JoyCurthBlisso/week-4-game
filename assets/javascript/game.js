0f43069e3800f609f565feda4d2a0426da6380e5
Joy Curth <joy.curth@comcast.net>
2018-01-10 20:39:35 -0500

    removed a few consoles and added document.ready

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,7 @@
+	 $(document).ready(function()
+	{
+
+
 	 var wins = 0;
      var losses =0;
      var totaUserNumber;
@@ -17,7 +21,6 @@
 
 			function setUserNumberText()
 			{
-				
 				var totalUserNumberText = document.getElementById("displayScore");
 				document.getElementById("displayScore").innerHTML =  totalUserNumber;
 				
@@ -26,29 +29,29 @@
 			setUserNumberText();
 
 			function randomNumber(min,max)
-				{
+			{
 				    return Math.floor(Math.random()*(max-min+1)+min);
-				}
+			}
 
 					computerRandomNumber = randomNumber(minNumber, maxNumber);
 					
 				
 				var computerRandomNumberText = document.getElementById("computerScore");
 				document.getElementById("computerScore").innerHTML =  computerRandomNumber;
-			
+				
 
 			 function setButtonNumber()
-			 	{
+			 {
 
 			     $(".btn").each(function(btnindex, btn)
 				     {
 						$(btn).data('value', randomNumber(1, 12));
 
 				     });
-			 	}
+			 }
 
 			 setButtonNumber();
-			}
+		}
 
 
        	$(".btn").on("click", function () 
@@ -56,7 +59,7 @@
        			var btn = $(this);
                 var btnValue = btn.data('value');
 
-				console.log(btnValue + "btnvalueclicked");	
+				
 
 				totalUserNumber = totalUserNumber + btnValue;
 
@@ -101,3 +104,4 @@
 					}	
 
 				}
+			});
